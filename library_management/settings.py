fe1b99129eea205419836516d988be1f2cbdd81c
anup30 <anupbarua30@gmail.com>
2025-08-09 21:31:59 +0600

    practice-26.5_v1.4

--- library_management/settings.py
+++ library_management/settings.py
@@ -151,13 +151,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Static files settings
-if not DEBUG:
-    STATIC_URL = '/static/'  # This matches public/static on Vercel
-else:
-    STATIC_URL = 'static/'
+
+STATIC_URL = '/static/' 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files settings
 MEDIA_URL = '/media/'
